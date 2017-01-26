23ab06a058bf5c16c82bc92d2fe2f0419d802c0e
Ben Van Citters <van.citters.b@gmail.com>
2017-01-26 00:48:52 -0800

    mobius

---
+++ 1-24-2017/New Unity Project 1/Assets/SPINNER.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPINNER : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(1.01f,1.2f,0.999f));
+	}
+}
